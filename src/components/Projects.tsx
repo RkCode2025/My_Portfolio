32d31613cb6df1af9eabd8d44d4fb2d4696a7ba1
Syphax <ss3240069@gmail.com>
2025-10-10 20:13:12 +0530

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Github, ExternalLink, BookOpen, Cpu, Database } from 'lucide-react';
+import { Github, ExternalLink, BookOpen, Cpu, Database, binary } from 'lucide-react';
 
 interface Project {
   title: string;
